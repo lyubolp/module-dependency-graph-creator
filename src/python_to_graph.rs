680952337d481a9923649634850d6001a4924eac
Lyuboslav Karev <lyubo.karev@gmail.com>
2025-08-29 08:16:49 +0300

    Refactor dependency graph functions for clarity and efficiency

--- src/python_to_graph.rs
+++ src/python_to_graph.rs
@@ -1,62 +1,56 @@
-use std::fs::{File, read};
-use std::io::{self, BufRead, Read};
-use std::str::FromStr;
-use petgraph::graph::{UnGraph};
-use regex::Regex;
-use crate::constants::{IMPORT};
+use std::{collections::HashSet, fs::read};
+// use petgraph::graph::{UnGraph};
 
 use ruff_python_parser;
+use ruff_python_ast::Stmt;
 
 pub fn build_dependency_graph(files: Vec<String>) {
 
-    let filepath=  &files[0];
+    for filepath in files {
+        let deps = get_all_dependencies(&filepath);
 
-    get_dependencies(filepath);
-    
-    // for item in get_dependencies(&files[0]).unwrap(){
-    //     //println!("{}", item);
-    //     println!("{} -> {:?}", item, extract_module(&item).unwrap());
-    // }
+        println!("{} -> {:?}", filepath, deps)
+    }
 }
 
 
-fn get_dependencies(filepath: &String) -> Result<Vec<String>, String> {
+fn get_all_dependencies(filepath: &String) -> HashSet<String> {
 
     let content = String::from_utf8(read(filepath).unwrap()).unwrap();
-
     let result = ruff_python_parser::parse_module(&content).unwrap();
 
+    let mut names: HashSet<String> = HashSet::new();
+
     for item in result.syntax().body.clone() {
-        if item.is_import_from_stmt(){
-            println!("{:?}", item);
-        }
-        else if item.is_import_stmt() {
-            println!("{:?}", item);
-        }
+        names.extend(extract_names(item.clone()).into_iter());
     }
 
-    
-    Ok(vec![])
+    names
 }
 
-fn is_line_import(line: &String) -> bool {
-    // TODO - This doesn't work for multiline imports
-    line.contains(IMPORT) && !line.contains("(")
-}
-
-fn extract_module(import_line: &String) -> Result<String, String> {
-    let re = Regex::new(r"(from (.*))?import ([^\d\W][\w.]*)").unwrap();
-
-    let Some(captures) = re.captures(&import_line) else {
-        return Err(String::from("Can't search line"));
-    };
-
-    let module = if import_line.contains("from") {
-        String::from_str(&captures[2].trim_end()).unwrap()
+fn extract_names(item: Stmt) -> HashSet<String> {
+    if item.is_import_from_stmt(){
+    extract_names_from_import_from_statement(item)
+    }
+    else if item.is_import_stmt() {
+        extract_names_from_import_statement(item)
     }
     else {
-        String::from_str(&captures[3].trim_end()).unwrap()
-    };
+        HashSet::new()
+    }
+}
 
-    Ok(module)
+fn extract_names_from_import_statement(item: Stmt) -> HashSet<String> {
+    item.import_stmt().unwrap().names.iter().map(|import| import.name.id.clone()).collect()
+}
+
+fn extract_names_from_import_from_statement(item: Stmt) -> HashSet<String> {
+    let statement = item.import_from_stmt().unwrap();
+
+    let Some(module) = statement.module else {
+        // TODO - This ignores imports from parent package
+        return HashSet::new();
+    };
+    
+    statement.names.iter().map(|alias| module.id.clone() + "." + &alias.name.id).collect()
 }
