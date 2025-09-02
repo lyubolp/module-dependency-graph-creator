2d377cae34953b1e1005d5e725d9cbb176cb91d2
Lyuboslav Karev <lyubo.karev@gmail.com>
2025-09-02 18:58:04 +0300

    Refactor dependency graph functions to include root directory parameter for improved context

--- src/python_to_graph.rs
+++ src/python_to_graph.rs
@@ -4,17 +4,19 @@ use std::{collections::HashSet, fs::read};
 use ruff_python_parser;
 use ruff_python_ast::Stmt;
 
-pub fn build_dependency_graph(files: Vec<String>) {
+use crate::python_utils::{extract_module_name, split_import};
+
+pub fn build_dependency_graph(files: Vec<String>, root_dir: &str) {
 
     for filepath in files {
-        let deps = get_all_dependencies(&filepath);
+        let deps = get_all_dependencies(&filepath, root_dir);
 
         println!("{} -> {:?}", filepath, deps)
     }
 }
 
 
-fn get_all_dependencies(filepath: &String) -> HashSet<String> {
+fn get_all_dependencies(filepath: &String, root_dir: &str) -> HashSet<String> {
 
     let content = String::from_utf8(read(filepath).unwrap()).unwrap();
     let result = ruff_python_parser::parse_module(&content).unwrap();
@@ -22,29 +24,44 @@ fn get_all_dependencies(filepath: &String) -> HashSet<String> {
     let mut names: HashSet<String> = HashSet::new();
 
     for item in result.syntax().body.clone() {
-        names.extend(extract_names(item.clone()).into_iter());
+        names.extend(extract_names(item.clone(), root_dir).into_iter());
     }
 
     names
 }
 
-fn extract_names(item: Stmt) -> HashSet<String> {
-    if item.is_import_from_stmt(){
-    extract_names_from_import_from_statement(item)
+fn extract_names(item: Stmt, root_dir: &str) -> HashSet<String> {
+    let names = if item.is_import_from_stmt(){
+        extract_names_from_import_from_statement(item)
     }
     else if item.is_import_stmt() {
         extract_names_from_import_statement(item)
     }
     else {
         HashSet::new()
-    }
+    };
+
+    names
+        .iter()
+        .map(|name|extract_module_name(name, root_dir))
+        .map(|name| split_import(&name).pop().unwrap_or(String::new()))
+        .filter(|name| name != "")
+        .collect()
 }
 
 fn extract_names_from_import_statement(item: Stmt) -> HashSet<String> {
-    item.import_stmt().unwrap().names.iter().map(|import| import.name.id.clone()).collect()
+    // TODO - This could return just an iterator
+    item
+        .import_stmt()
+        .unwrap()
+        .names
+        .iter()
+        .map(|import| import.name.id.clone())
+        .collect()
 }
 
 fn extract_names_from_import_from_statement(item: Stmt) -> HashSet<String> {
+    // TODO - This could return just an iterator
     let statement = item.import_from_stmt().unwrap();
 
     let Some(module) = statement.module else {
@@ -52,5 +69,10 @@ fn extract_names_from_import_from_statement(item: Stmt) -> HashSet<String> {
         return HashSet::new();
     };
     
-    statement.names.iter().map(|alias| module.id.clone() + "." + &alias.name.id).collect()
+    statement
+        .names
+        .iter()
+        .map(|alias| module.id.clone() + "." + &alias.name.id)
+        .collect()
 }
+
