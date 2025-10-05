58d641dfca32e6ccbefe523bb7000c47057cc50b
Lyuboslav Karev <lyubo.karev@gmail.com>
2025-10-06 00:08:59 +0300

    First PlantUML content generation

--- src/graph_to_uml.rs
+++ src/graph_to_uml.rs
@@ -1,5 +1,37 @@
-use crate::graph::Graph;
+use crate::{graph::Graph, module::PythonModule};
 
-pub fn generate_plantuml(graph: &Graph) -> String {
-    unimplemented!()
+pub fn generate_plantuml(graph: &Graph<PythonModule>) -> Vec<String> {
+    let colors: Vec<&str> = vec![
+        "#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f",
+        "#bcbd22", "#17becf",
+    ];
+
+    let mut result: Vec<String> = vec![];
+
+    result.push(String::from("@startuml"));
+
+    result.push(String::from("skinparam packageStyle rectangle"));
+    result.push(String::from("skinparam linetype ortho"));
+    result.push(String::from("skinparam class {"));
+    result.push(String::from("    BackgroundColor White"));
+    result.push(String::from("    BorderColor Black"));
+    result.push(String::from("}"));
+    result.push(String::from("left to right direction"));
+
+    // TODO - Probably need a tree representation of the modules to properly nest them
+
+    for node in graph.get_nodes() {
+        if let Ok(edges) = graph.get_edges(node) {
+            for edge in edges {
+                let content = String::from(&format!(
+                    "[\"{}\"] --> [\"{}\"]",
+                    node.get_name(),
+                    edge.get_name()
+                ));
+                result.push(content);
+            }
+        }
+    }
+
+    result
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
-mod python_to_graph;
-mod python_utils;
-mod file_utils;
 mod constants;
+mod file_utils;
 mod graph;
 mod graph_to_uml;
 mod module;
+mod python_to_graph;
+mod python_utils;
 
 use crate::file_utils::discover_files;
+use crate::graph_to_uml::generate_plantuml;
 use crate::python_to_graph::build_dependency_graph;
 
 fn main() {
@@ -15,10 +16,15 @@ fn main() {
 
     let graph = build_dependency_graph(files, root_dir);
 
+    let content = generate_plantuml(&graph);
+
     for node in graph.get_nodes() {
         if let Ok(edges) = graph.get_edges(node) {
             println!("{} -> {:?}", node, edges);
         }
+    }
 
+    for line in content {
+        println!("{}", line)
     }
 }
