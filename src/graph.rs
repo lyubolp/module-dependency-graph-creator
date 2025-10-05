55df49d2a9336e50250e13a2a9cd025c3e0255f4
Lyuboslav Karev <lyubo.karev@gmail.com>
2025-10-05 23:53:08 +0300

    Graph is now generic

--- src/graph.rs
+++ src/graph.rs
@@ -1,35 +1,40 @@
-use std::collections::{HashMap};
+use std::cmp::Eq;
+use std::collections::HashMap;
+use std::hash::Hash;
 
 #[derive(Debug, Clone)]
-pub struct Graph{
-    node_to_id: HashMap<String, u32>,
-    id_to_node: HashMap<u32, String>,
-    edges: HashMap<u32, Vec<u32>>
+pub struct Graph<T: Clone + Eq + Hash> {
+    node_to_id: HashMap<T, u32>,
+    id_to_node: HashMap<u32, T>,
+    edges: HashMap<u32, Vec<u32>>,
 }
 
-impl Graph {
+impl<T: Clone + Eq + Hash> Graph<T> {
     pub fn new() -> Self {
-        Graph { node_to_id: HashMap::new(), id_to_node: HashMap::new(), edges: HashMap::new() }
+        Graph {
+            node_to_id: HashMap::new(),
+            id_to_node: HashMap::new(),
+            edges: HashMap::new(),
+        }
     }
 
-    pub fn add_node(&mut self, node: &String) -> Result<&str, &str> {
-        if !self.is_node_in_graph(&node) {
+    pub fn add_node(&mut self, node: &T) -> Result<&str, &str> {
+        if !self.is_node_in_graph(node) {
             let current_index = self.node_to_id.len() as u32;
             self.node_to_id.insert(node.clone(), current_index);
             self.id_to_node.insert(current_index, node.clone());
-    
+
             Ok("Node added succesfully!")
-        }
-        else {
+        } else {
             Err("Node already in graph.")
         }
     }
 
-    pub fn is_node_in_graph(&self, node: &String) -> bool {
+    pub fn is_node_in_graph(&self, node: &T) -> bool {
         self.node_to_id.contains_key(node)
     }
 
-    pub fn add_edge(&mut self, start: &String, end: &String) -> Result<&str, &str> {
+    pub fn add_edge(&mut self, start: &T, end: &T) -> Result<&str, &str> {
         if !self.is_node_in_graph(start) {
             return Err("Start not in graph");
         }
@@ -47,15 +52,14 @@ impl Graph {
 
         if self.edges.contains_key(&start_id) {
             self.edges.get_mut(&start_id).unwrap().push(end_id);
-        }
-        else {
+        } else {
             self.edges.insert(start_id, vec![end_id]);
         }
 
         Ok("Edge added successfully")
     }
 
-    pub fn does_edge_exist(&self, start: &String, end: &String) -> bool {
+    pub fn does_edge_exist(&self, start: &T, end: &T) -> bool {
         if !self.is_node_in_graph(start) || !self.is_node_in_graph(end) {
             return false;
         }
@@ -69,24 +73,19 @@ impl Graph {
         return self.edges[&start_id].contains(&end_id);
     }
 
-    pub fn get_edges(&self, node: &String) -> Result<Vec<String>, &str> {
+    pub fn get_edges(&self, node: &T) -> Result<Vec<T>, &str> {
         if !self.is_node_in_graph(node) {
             return Err("Node not in graph");
         }
         let node_id = self.node_to_id[node];
 
         match self.edges.get(&node_id) {
-            Some(edges) => {
-                Ok(edges
-                    .iter()
-                    .map(|id| self.id_to_node[id].clone())
-                    .collect())
-            }
-            None => Err("No edges found")
+            Some(edges) => Ok(edges.iter().map(|id| self.id_to_node[id].clone()).collect()),
+            None => Err("No edges found"),
         }
     }
 
-    pub fn get_nodes(&self) -> impl Iterator<Item = &String> {
+    pub fn get_nodes(&self) -> impl Iterator<Item = &T> {
         self.node_to_id.keys()
     }
 }
