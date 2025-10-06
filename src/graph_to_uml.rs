fcc2dcaf8a0be79531c79c8425bc9178bc871e6b
Lyuboslav Karev <lyubo.karev@gmail.com>
2025-10-06 09:46:43 +0300

    Add element definition

--- src/graph_to_uml.rs
+++ src/graph_to_uml.rs
@@ -10,6 +10,8 @@ pub fn generate_plantuml(graph: &Graph<PythonModule>) -> Vec<String> {
 
     result.push(String::from("@startuml"));
 
+    result.push(String::from(""));
+
     result.push(String::from("skinparam packageStyle rectangle"));
     result.push(String::from("skinparam linetype ortho"));
     result.push(String::from("skinparam class {"));
@@ -18,20 +20,30 @@ pub fn generate_plantuml(graph: &Graph<PythonModule>) -> Vec<String> {
     result.push(String::from("}"));
     result.push(String::from("left to right direction"));
 
+    result.push(String::from(""));
     // TODO - Probably need a tree representation of the modules to properly nest them
 
     for node in graph.get_nodes() {
+        let content = String::from(&format!("[\"{}\"]", node.get_name()));
+        result.push(content);
+    }
+    result.push(String::from(""));
+
+    for node in graph.get_nodes() {
         if let Ok(edges) = graph.get_edges(node) {
-            for edge in edges {
+            for (i, edge) in edges.iter().enumerate() {
                 let content = String::from(&format!(
-                    "[\"{}\"] --> [\"{}\"]",
+                    "[\"{}\"] -[{}]-> [\"{}\"]",
                     node.get_name(),
+                    colors[i % colors.len()],
                     edge.get_name()
                 ));
                 result.push(content);
             }
+            result.push(String::from(""));
         }
     }
 
+    result.push(String::from("@enduml"));
     result
 }
